ba254f58533fbcb261573915808af403617318ca
atellmer <atellmer@gmail.com>
2017-07-05 19:04:41 +0500

    first commit

---
+++ src/graphql/resolvers.ts
@@ -0,0 +1,34 @@
+const channels = [
+  {
+    id: 1,
+    name: 'soccer'
+  }, {
+    id: 2,
+    name: 'baseball'
+  }, {
+    id: 3,
+    name: 'boxing'
+  }
+];
+
+let nextId = 4;
+
+export const resolvers = {
+  Query: {
+    channels: () => {
+      return channels;
+    }
+  },
+  Mutation: {
+    addChannel: (root, args) => {
+      const newChannel = {
+        id: nextId++,
+        name: args.name
+      };
+
+      channels.push(newChannel);
+
+      return newChannel;
+    }
+  }
+};
